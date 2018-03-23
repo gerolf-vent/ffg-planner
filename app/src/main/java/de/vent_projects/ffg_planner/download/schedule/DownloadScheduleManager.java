8819276287c19bd2e460807d50b988813ae67942
Gerolf Vent <gerolf.vent@gmx.de>
2018-03-23 21:03:54 +0100

    Kommentare hinzugefügt Variablennamen angeglichen

--- app/src/main/java/de/vent_projects/ffg_planner/download/schedule/DownloadScheduleManager.java
+++ app/src/main/java/de/vent_projects/ffg_planner/download/schedule/DownloadScheduleManager.java
@@ -34,6 +34,13 @@ import io.realm.RealmResults;
 
 import static de.vent_projects.ffg_planner.CommonUtils.isStringBlank;
 
+/**
+ * Der DownloadScheduleManager hat verschiedene Aufgaben:
+ *  - Herunterladen des Stundenplanes
+ *  - Abfragen in Realm
+ *  - Vorbereiten der Daten für das Setup
+ */
+
 public class DownloadScheduleManager extends ScheduleManager {
     private static final String TAG = DownloadScheduleManager.class.getSimpleName();
 
@@ -46,31 +53,46 @@ public class DownloadScheduleManager extends ScheduleManager {
         this.settingsConfigurationManager = settingsConfigurationManager;
     }
 
-    // GET LESSONS
+    // ABFRAGEN DER STUNDEN
 
+    // Gibt alle Stunden für einen Tag und eine Stunde aus
     public RealmResults<DownloadLesson> getAllLessonsForDayAndPeriod(int day, int period) {
         return getRealm().where(DownloadLesson.class).equalTo("day", day).equalTo("period", period).findAll();
     }
 
+    /*
+     * Gibt alle A-Stunden aus.
+     * Dies ist nicht nur für die 10. Klassen relevant!
+     */
     public RealmResults<DownloadLesson> getAllALessonsForDayAndPeriod(int day, int period) {
         return  getRealm().where(DownloadLesson.class).equalTo("day", day).equalTo("period", period).not().contains("subject.teacher", "**").findAll();
     }
 
+    // Gibt alle B-Stunden aus.
     public RealmResults<DownloadLesson> getAllBLessonsForDayAndPeriod(int day, int period) {
         return  getRealm().where(DownloadLesson.class).equalTo("day", day).equalTo("period", period).contains("subject.teacher", "**").findAll();
     }
 
+    // Hier wird abgefragt, ob die jeweilige Stunde eine A/B-Stunde ist
     public boolean isABLessonForDayAndPeriod(int day, int period) {
         return getRealm().where(DownloadLesson.class).equalTo("day", day).equalTo("period", period).contains("subject.teacher", "*").count() > 0;
     }
 
+    /*
+     * Hier wird nach gleichen Stundennamen gesucht.
+     * Dies ist für die automatische Auswahl der Kurse im Setup notwendig, jedoch nur für die Oberstufe relevant.
+     */
     public RealmResults<DownloadLesson> getLessonsWithSimilarSubjectName(String name) {
         if (isStringBlank(name)) return null;
         return getRealm().where(DownloadLesson.class).equalTo("subject.name", name).findAll();
     }
 
-    // SOME SUBJECTS ARE POSSIBLE
-
+    /*
+     * Dies ist eine extrem wichtige Funktion!
+     * Sie steuert, ob eine Stunde als optional angesehen wird. Dies ist z.B. bei Wahlpflicht der Fall oder bei Förderunterricht.
+     * Wenn die Funktion true zurückgibt, wird beim Transferer (de.vent_project.ffg-planner.schedule.transferer.ScheduleTransferer) diese Stunde
+     * ignoriert und alle anderen Stunden bevorzugt behandelt. Für weitere Informationen bitte die Kommentare des ScheduleTransferers ansehen.
+     */
     public boolean isLessonPossible(DownloadLesson lesson) {
         return  lesson.getName().toLowerCase().contains("fö") ||
                 lesson.getName().toLowerCase().contains("wp") ||
@@ -78,8 +100,11 @@ public class DownloadScheduleManager extends ScheduleManager {
                 lesson.getName().equals("daz");
     }
 
-    // SET LESSONS
+    // BEARBEITUNG DER STUNDEN
 
+    /* Löscht die alle heruntergeladen Stundenplan-Daten
+     * TODO: Hochlade- und Gültigkeitsdatum ebenfalls löschen (DownloadSettingsManager)
+     */
     private void clearSchedule() {
         Realm realm = getRealm();
         realm.beginTransaction();
@@ -89,8 +114,13 @@ public class DownloadScheduleManager extends ScheduleManager {
         realm.close();
     }
 
-    // REMOVE UNSELECTED SUBJECTS
+    // ENTFERNUNG UNAUSGEWÄHLTER FÄCHER
 
+    /*
+     * Dies ist nur für das Setup relevant.
+     * Diese Funktion entfernt alle im CommonFragment (de.vent_projects.ffg_planner.setup.ui.common.SetupCommonFragment)
+     * nicht ausgewählten optionen und erleichtert somit die Arbeit des ScheduleTransferers (de.vent_project.ffg-planner.schedule.transferer.ScheduleTransferer).
+     */
     public void removeUnselectedSubjects() {
         SchoolClass schoolClass = this.settingsConfigurationManager.getSchoolClass();
         if (schoolClass.isUpperLevel()) return;
@@ -100,11 +130,22 @@ public class DownloadScheduleManager extends ScheduleManager {
         unselectedSubjectNames.remove(settingsConfigurationManager.getLanguage());
         unselectedSubjectNames.addAll(Arrays.asList(getContext().getResources().getStringArray(R.array.options_believes_values)));
         unselectedSubjectNames.remove(settingsConfigurationManager.getBelieve());
+
+        /*
+         * Es gibt nun an der Schule neben Spanisch auch Griechisch als Drittsprache.
+         * Hier wird also die nicht ausgewählte Sprache entfernt oder beide falls keine zweite Fremdsprache ausgewählt wurde.
+         * Dies ist auch nur für die 10. Klassen relevant. Hier jedoch aus kompatibilitätsgründen schon gleich auf alle Klassen angewandt.
+         * Falls es bei den Drittsprachen Änderungen gibt muss diese Stelle und das SetupCommonFragment (de.vent_projects.ffg_planner.setup.ui.common.SetupCommonFragment)
+         * bearbeitet werden.
+         */
         unselectedSubjectNames.addAll(Arrays.asList(getContext().getResources().getStringArray(R.array.options_third_languages_values)));
         unselectedSubjectNames.remove("none");
         unselectedSubjectNames.remove(settingsConfigurationManager.getThirdLanguage());
-        /*if (schoolClass.getGrade() == 10 && !settingsConfigurationManager.isSpanish()) {
-            unselectedSubjectNames.add(getContext().getString(R.string.lesson_spanish_abbreviation));
+        /*
+         * Hier ist aus Einfachheit die alte Vorgehensweise noch einmal auskommentiert.
+         *
+         * if (schoolClass.getGrade() == 10 && !settingsConfigurationManager.isSpanish()) {
+         *    unselectedSubjectNames.add(getContext().getString(R.string.lesson_spanish_abbreviation));
         }*/
 
         Realm realm = getRealm();
@@ -113,71 +154,92 @@ public class DownloadScheduleManager extends ScheduleManager {
         realm.commitTransaction();
     }
 
-    // DOWNLOAD SCHEDULE FROM SCHOOL SERVER
+    // HERUNTERLADEN DES STUNDENPLANES
 
+    /*
+     * Hier wird der Stundenplan heruntergeladen und in direkt in Realm abgespeichert.
+     *
+     * Bitte diese Funktion niemals direkt benutzen sondern deren asynchrone variante (siehe unten), ansonsten wird der Main-Thread oder
+     * der Thread, der gerade genutzt wird blockiert, bis der Download komplett ist oder abgebrochen wurde.
+     *
+     * Dabei gibt es für den Stundenplan der Sekundarstufe 1 & 2 unterschiedliche Namensformate, welche unten
+     * separat gebildet werden.
+     *
+     * Als Parser der XML-Dateien wird der XMLPullParser genutzt, welcher bei Events (z.B. bei einem Öffnen-/Schließen-Tag,
+     * Text oder Attributen) verschieden reagieren kann. Durch dieses Prinzip werden erst die Daten in die DownloadLesson
+     * eingefügt und beim Schließen-Tag die DownloadLesson abgespeichert.
+     */
     private void downloadScheduleFromServer(DownloadScheduleListener listener){
         clearSchedule();
         Realm realm = getRealm();
         String urlStr;
         SchoolClass schoolClass = this.settingsConfigurationManager.getSchoolClass();
+        //Generierung der URL
         if (schoolClass.isUpperLevel()){
             urlStr = "http://www.ffg-dbr.de/plaene/stundenplan/iiplank"+Integer.toString(schoolClass.getGrade())+".xml";
         } else {
             urlStr = "http://www.ffg-dbr.de/plaene/stundenplan/iiplank"+Integer.toString(schoolClass.getGrade())+Character.toUpperCase(schoolClass.getName())+".xml";
         }
         try {
+            // Aufbauen der Verbindung
             URL url = new URL(urlStr);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             try {
                 if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    // Vorbereiten des Ausgabestreams und des XMLPullParsers
                     InputStream responseData = urlConnection.getInputStream();
                     XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
                     XmlPullParser xpp = factory.newPullParser();
                     if (responseData != null) {
+                        // Eingabe der Daten in den XMLPullParser
                         xpp.setInput(responseData, "ISO-8859-1");
                         int eventType = xpp.getEventType();
+                        // Dies sind Zwischenspeicher für die Daten während des parsens.
                         String text = "";
                         DownloadLesson lesson = new DownloadLesson();
                         Integer period = 1;
+                        // Realm wird zum schreiben geöffnet, um die DownloadLessons direkt abspeichern zu können.
                         realm.beginTransaction();
                         while (eventType != XmlPullParser.END_DOCUMENT) {
-                            String tag_name = xpp.getName();
+                            String tagName = xpp.getName();
                             if (eventType == XmlPullParser.START_TAG) {
-                                if (tag_name.contains("tag")) {
-                                    Matcher dayNumber = Pattern.compile("[0-9]+").matcher(tag_name);
+                                if (tagName.contains("tag")) {
+                                    Matcher dayNumber = Pattern.compile("[0-9]+").matcher(tagName);
                                     lesson = new DownloadLesson();
                                     if (dayNumber.find()) {
                                         lesson.setDay(Integer.parseInt(dayNumber.group()) + 1);
                                         lesson.setPeriod(period);
                                     } else {
-                                        Log.e(TAG, "No day number is available in XML tag '" + tag_name + "'");
+                                        Log.e(TAG, "No day number was found in XML tag '" + tagName + "'");
                                     }
                                 }
                             } else if (eventType == XmlPullParser.TEXT) {
                                 text = xpp.getText();
                             } else if (eventType == XmlPullParser.END_TAG) {
-                                if (tag_name.equals("stunde")) {
+                                if (tagName.equals("stunde")) {
                                     period = Integer.parseInt(text);
-                                } else if (tag_name.equals("fach")) {
+                                } else if (tagName.equals("fach")) {
                                     lesson.setName(text);
-                                } else if (tag_name.equals("lehrer")) {
+                                } else if (tagName.equals("lehrer")) {
                                     lesson.setTeacher(text);
-                                } else if (tag_name.equals("raum")) {
+                                } else if (tagName.equals("raum")) {
                                     lesson.setRoom(text);
-                                } else if (tag_name.equals("gruppe")) {
+                                } else if (tagName.equals("gruppe")) {
                                     if (!isStringBlank(text)) {
                                         lesson.setName(text);
                                     }
-                                } else if (tag_name.contains("tag")) {
-                                    Matcher dayNumber = Pattern.compile("[0-9]+").matcher(tag_name);
+                                } else if (tagName.contains("tag")) {
+                                    Matcher dayNumber = Pattern.compile("[0-9]+").matcher(tagName);
                                     if (dayNumber.find()) {
                                         if (Integer.parseInt(dayNumber.group()) <= 5) {
                                             realm.copyToRealm(lesson);
                                         }
                                     }
-                                } else if (tag_name.equals("datum")) {
+                                } else if (tagName.equals("datum")) {
+                                    // Hier wird das Hochlade-Datum des Stundenplans abgespeichert.
                                     this.settingsManager.setScheduleUploadDate(CommonUtils.toDate(text));
-                                } else if (tag_name.equals("gueltigab")) {
+                                } else if (tagName.equals("gueltigab")) {
+                                    // Hier wird das Fültigkeits-Datum des Stundenplanes abgespeichert, welches den Tag des In-Kraft-tretens beschreibt.
                                     this.settingsManager.setScheduleValidityDate(CommonUtils.toDate(text));
                                 }
                             }
@@ -214,6 +276,10 @@ public class DownloadScheduleManager extends ScheduleManager {
         }
     }
 
+    /*
+     * Hier wird der Download des Stundenplans asynchron im Hintergrund durchgeführt.
+     * Dabei wird gleich auch die Connectivität geprüft, um keine sinnlosen Request zu senden.
+     */
     private static class DownloadScheduleAsyncTask extends AsyncTask<Void, Void, Void> {
         private DownloadScheduleListener listener;
         private DownloadScheduleManager scheduleManager;
@@ -238,6 +304,10 @@ public class DownloadScheduleManager extends ScheduleManager {
         }
     }
 
+    /*
+     * Dies hier ist die nutzbare Funktion zum download des Stundenplans
+     * Sie startet einen AsyncTask, welcher dann den Download durchführt.
+     */
     public void downloadScheduleFromServerAsync(DownloadScheduleListener listener) {
         new DownloadScheduleAsyncTask(this, listener).execute();
     }
